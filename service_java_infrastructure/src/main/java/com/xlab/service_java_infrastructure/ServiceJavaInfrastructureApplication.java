96364bde9f3e38c2624cbe8beaf50af07d5c7cc3
居洪涛 <hongtao.ju@blockchain001.com>
2018-06-16 16:21:13 +0800

    initial project

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/ServiceJavaInfrastructureApplication.java
@@ -0,0 +1,12 @@
+package com.xlab.service_java_infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJavaInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceJavaInfrastructureApplication.class, args);
+	}
+}
---
+++ service_java_infrastructure/src/test/java/com/xlab/service_java_infrastructure/ServiceJavaInfrastructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xlab.service_java_infrastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceJavaInfrastructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
