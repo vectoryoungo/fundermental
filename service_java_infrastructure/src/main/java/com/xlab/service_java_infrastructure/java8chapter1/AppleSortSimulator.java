23f69932d9b4acbfb2e31d7853eab2d7f04a3522
居洪涛 <hongtao.ju@blockchain001.com>
2018-06-17 16:11:56 +0800

    add new function and comment

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter1/AppleSortSimulator.java
@@ -0,0 +1,70 @@
+/*
+ * module: ${PROJECT_NAME}
+ * file: ${FILE_NAME}
+ * date: ${DATE}
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-06-17 15:02
+ * @desc sort apples
+ **/
+package com.xlab.service_java_infrastructure.java8chapter1;
+
+import sun.jvm.hotspot.debugger.ThreadAccess;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AppleSortSimulator {
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
+                new Apple(155,"brown"),
+                new Apple(80,"grey"),
+                new Apple(155, "green"),
+                new Apple(120, "red"));
+
+        inventory.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+        });
+
+
+        //traditional thread in java
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello this is traditional thread run....");
+            }
+        });
+
+        //thread.start();
+
+        //lambda thread in java8
+        Thread lambdaThread = new Thread(()->System.out.println("hello this is lambda thread run..."));
+
+        //lambdaThread.start();
+        //() -> {return "Mario";}
+        Runnable r1 = () -> System.out.println("Hello World 1");
+        Runnable r2 = new Runnable(){
+            public void run(){
+                System.out.println("Hello World 2");
+            }
+        };
+        process(r1);
+        process(r2);
+        process(()->System.out.println("Hello World 3"));
+
+
+    }
+
+    public static void process(Runnable r){
+        r.run();
+    }
+}
+
---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter1/Comparator.java
@@ -0,0 +1,20 @@
+/*
+ * module: ${PROJECT_NAME}
+ * file: ${FILE_NAME}
+ * date: ${DATE}
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-06-17 15:00
+ * @desc sort with comparator
+ **/
+package com.xlab.service_java_infrastructure.java8chapter1;
+
+public interface Comparator<T> {
+
+    int compare(T o1,T o2);
+
+}
+
