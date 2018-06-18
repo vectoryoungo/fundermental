0d752322524a529eda98e80dcc886c29536c5386
居洪涛 <hongtao.ju@blockchain001.com>
2018-06-18 09:54:11 +0800

    add complex  functional method

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter1/FunctionCompose.java
@@ -0,0 +1,27 @@
+/*
+ * module: ${PROJECT_NAME}
+ * file: ${FILE_NAME}
+ * date: ${DATE}
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-06-17 22:23
+ * @desc compose function
+ **/
+package com.xlab.service_java_infrastructure.java8chapter1;
+
+import java.util.function.Function;
+
+public class FunctionCompose {
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> f = x -> x + 1;
+        Function<Integer, Integer> g = x -> x * 2;
+        Function<Integer, Integer> h = f.compose(g);
+        int result = h.apply(1);
+        System.out.println(result);
+    }
+}
+
