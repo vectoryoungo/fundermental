39c792985091a29e177e1ff3e22264dee55e93bc
居洪涛 <hongtao.ju@blockchain001.com>
2018-06-19 08:29:30 +0800

    add data value stream test

--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter5/BestPractiseStreamUse.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter5/BestPractiseStreamUse.java
@@ -12,6 +12,8 @@
  **/
 package com.xlab.service_java_infrastructure.java8chapter5;
 
+import com.xlab.service_java_infrastructure.java8chapter4.Dish;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -23,6 +25,16 @@ import static java.util.stream.Collectors.toSet;
 public class BestPractiseStreamUse {
 
     public static void main(String[] args) {
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH) );
         Trader raoul = new Trader("Raoul", "Cambridge");
         Trader mario = new Trader("Mario","Milan");
         Trader alan = new Trader("Alan","Cambridge");
@@ -98,6 +110,22 @@ public class BestPractiseStreamUse {
         Optional<Transaction> smallestTransactions =
                 transactions.stream()
                         .min(comparing(Transaction::getValue));
+
+       //data value stream
+        long startCaloriesCalculate = System.currentTimeMillis();
+        int calories = menu.stream()
+                .map(Dish::getCalories)
+                .reduce(0, Integer::sum);
+        System.out.println("calories is " + calories);
+        System.out.println(" calories without optimized is " + (System.currentTimeMillis() - startCaloriesCalculate));
+
+        //mapped to data value stream
+        long startCaloriesWithOptimized = System.currentTimeMillis();
+        int caloriesOptimized = menu.stream()
+                .mapToInt(Dish::getCalories)
+                .sum();
+        System.out.println("optimized data value " + caloriesOptimized);
+        System.out.println(" optimized data value takes " + (System.currentTimeMillis() - startCaloriesWithOptimized));
     }
 }
 
