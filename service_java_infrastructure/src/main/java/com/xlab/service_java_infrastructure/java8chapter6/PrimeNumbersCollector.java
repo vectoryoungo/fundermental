e796cf07366db33376a7a26af8f7f88d2299bce9
居洪涛 <hongtao.ju@blockchain001.com>
2018-06-20 11:41:02 +0800

    add new example

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter6/PrimeNumbersCollector.java
@@ -0,0 +1,70 @@
+/*
+ * module: ${PROJECT_NAME}
+ * file: ${FILE_NAME}
+ * date: ${DATE}
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-06-20 11:08
+ * @desc demo for self collector
+ **/
+package com.xlab.service_java_infrastructure.java8chapter6;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collector.Characteristics.IDENTITY_FINISH;
+
+public class PrimeNumbersCollector implements Collector<Integer,
+        Map<Boolean, List<Integer>>,
+        Map<Boolean, List<Integer>>> {
+
+
+    @Override
+    public Supplier<Map<Boolean, List<Integer>>> supplier() {
+        return  ()->new HashMap<Boolean, List<Integer>>(){{
+            put(true,new ArrayList<Integer>());
+            put(false,new ArrayList<Integer>());
+        }};
+    }
+
+    @Override
+    public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+        return (Map<Boolean, List<Integer>> acc,Integer candidate)->{
+            acc.get(SelfDefinitionCollectorImprovePerformace.isPrime(acc.get(true),candidate)).add(candidate);
+        };
+    }
+
+    @Override
+    public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+        return (Map<Boolean,List<Integer>> map1,Map<Boolean,List<Integer>> map2)->{
+            map1.get(true).addAll(map2.get(true));
+            map1.get(false).addAll(map2.get(false));
+            return map1;
+        };
+    }
+
+    @Override
+    public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH));
+    }
+
+    public Map<Boolean, List<Integer>>
+    partitionPrimesWithCustomCollector(int n) {
+        return IntStream.rangeClosed(2, n).boxed()
+                .collect(new PrimeNumbersCollector());
+    }
+}
+
---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter6/SelfDefinitionCollectorImprovePerformace.java
@@ -0,0 +1,48 @@
+/**
+ * @author juhongtao
+ * @create 2018-06-20 11:00
+ * @desc self collector improve
+ **/
+package com.xlab.service_java_infrastructure.java8chapter6;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.partitioningBy;
+
+public class SelfDefinitionCollectorImprovePerformace {
+
+    public Map<Boolean, List<Integer>> partitionPrimes(int n) {
+        return IntStream.rangeClosed(2, n).boxed()
+                .collect(partitioningBy(candidate -> isPrime(candidate)));
+    }
+
+    public boolean isPrime(int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return IntStream.rangeClosed(2, candidateRoot)
+                .noneMatch(i -> candidate % i == 0);
+    }
+
+    public static boolean isPrime(List<Integer> primes, int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return takeWhile(primes, i -> i <= candidateRoot)
+                .stream()
+                .noneMatch(p -> candidate % p == 0);
+        //return primes.stream().noneMatch(i -> candidate % i == 0);
+    }
+
+    //给定一个排序列表和一个谓词,返回元素满足谓词的最长前缀
+    public static <A> List<A> takeWhile(List<A> list, Predicate<A> p) {
+        int i = 0;
+        for (A item : list) {
+            if (!p.test(item)) {
+                return list.subList(0, i);
+            }
+            i++;
+        }
+        return list;
+    }
+}
+
