ec044c69746cc8a8c746e9a78dc9e4332ff19667
居洪涛 <hongtao.ju@blockchain001.com>
2018-06-29 09:05:41 +0800

    add chapter8 example

--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter8/RefactorSimulator.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/java8chapter8/RefactorSimulator.java
@@ -12,6 +12,12 @@
  **/
 package com.xlab.service_java_infrastructure.java8chapter8;
 
+import com.xlab.service_java_infrastructure.java8chapter5.Trader;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Comparator;
+
 public class RefactorSimulator {
 
     public static void main(String[] args) {
@@ -27,6 +33,17 @@ public class RefactorSimulator {
         }
         };
         //new Thread(r2).start();
+
+        Integer commissionPercent = Integer.MAX_VALUE;
+        Double commissionPercentDouble=commissionPercent*0.01;
+        BigDecimal result = new BigDecimal("1.235").multiply(new BigDecimal(commissionPercentDouble));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(commissionPercentDouble.toString());
+        System.out.println(result.toPlainString());
+        System.out.println(new BigDecimal(commissionPercentDouble.toString()).toPlainString());
+        Comparator<Trader> comparator = Comparator.comparing(Trader::getName).reversed();
+
+
     }
 }
 
