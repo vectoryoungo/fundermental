045633c436be30dd8679a79f7d5083bd283e2b0d
居洪涛 <hongtao.ju@blockchain001.com>
2018-08-25 23:07:43 +0800

    add concurrent

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/effective8/ConcurrentLabWithStop.java
@@ -0,0 +1,17 @@
+/*
+ * module: fundermental
+ * file: ConcurrentLabWithStop.java
+ * date: 18-8-25 下午11:06
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-08-25 23:06
+ * @desc current test with stop
+ **/
+package com.xlab.service_java_infrastructure.effective8;
+
+public class ConcurrentLabWithStop {
+}
+
---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/effective8/chapter78/ConcurrentLab.java
@@ -0,0 +1,34 @@
+/*
+ * module: fundermental
+ * file: ConcurrentLab.java
+ * date: 18-8-25 下午8:47
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-08-25 20:47
+ * @desc 测试并发停止另一个线程
+ **/
+package com.xlab.service_java_infrastructure.effective8.chapter78;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConcurrentLab {
+
+    private static boolean stopRequested;
+
+    public static void main(String[] args) throws InterruptedException{
+        Thread backgroundThread = new Thread(()->{
+            int i=0;
+            while (!stopRequested) {
+                i++;
+            }
+        });
+
+        backgroundThread.start();
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
+    }
+}
+
