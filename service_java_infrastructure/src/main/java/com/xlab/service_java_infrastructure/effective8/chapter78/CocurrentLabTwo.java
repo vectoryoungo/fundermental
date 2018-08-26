6945d14d21cf63005baa930d184f36b19440735c
居洪涛 <hongtao.ju@blockchain001.com>
2018-08-26 15:37:20 +0800

    add functiom

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/effective8/chapter78/CocurrentLabTwo.java
@@ -0,0 +1,27 @@
+/*
+ * module: fundermental
+ * file: CocurrentLabTwo.java
+ * date: 18-8-25 下午11:36
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-08-25 23:35
+ * @desc with no stop
+ **/
+package com.xlab.service_java_infrastructure.effective8.chapter78;
+
+public class CocurrentLabTwo {
+
+    private static volatile int nextSerialNumber =0;
+
+    public static int generateSerialNumber() {
+        return nextSerialNumber++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
