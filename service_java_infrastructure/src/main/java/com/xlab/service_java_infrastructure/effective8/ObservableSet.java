6a20fe2c0b478a60bb238fda318dcf6ee9f02aea
居洪涛 <hongtao.ju@blockchain001.com>
2018-08-27 11:33:07 +0800

    add deadlock comment

--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/effective8/ObservableSet.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/effective8/ObservableSet.java
@@ -13,6 +13,9 @@
 package com.xlab.service_java_infrastructure.effective8;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ObservableSet<E> extends ForwardingSet<E> {
 
@@ -67,17 +70,34 @@ public class ObservableSet<E> extends ForwardingSet<E> {
             public void added(ObservableSet<Integer> set, Integer element) {
                 System.out.println(element);
                 if (element == 23) {
-                    set.removeObserver(this);
+                    //set.removeObserver(this);
+                    // this will lead in thread "main" java.util.ConcurrentModificationException
+                    // because notifyElementAdded is in the process of iterating over the observers list
+                    // when it invokes the observer's added method
+                    ExecutorService exec =
+                            Executors.newSingleThreadExecutor();
+                    try {
+                        exec.submit(() -> set.removeObserver(this)).get();
+                    } catch (ExecutionException | InterruptedException ex) {
+                        throw new AssertionError(ex);
+                    } finally {
+                        exec.shutdown();
+                    }
                 }
             }
         });
 
         for(int i = 0;i < 100;i++) {
             set.add(i);
-        }}
-        // this will lead in thread "main" java.util.ConcurrentModificationException
-        // because notifyElementAdded is in the process of iterating over the observers list
-        // when it invokes the observer's added method.
+        }
+    }
+        // comment set.removeObserver(this) user ExecutorService this lead to
+        // deadlock.The background thread calls set.removeObserver(this),which
+        // attempts to lock observers,but it can't acquire the lock,because the main
+        // thread already has the lock. All the while,the main thread is waiting for
+        //the background thread to finish removing the observer , which explain the deadlock
+
+
 
 }
 
