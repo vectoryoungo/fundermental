73a8cbccd496f42efb695876d0ab14fb7cf2d6f3
居洪涛 <hongtao.ju@blockchain001.com>
2018-12-24 17:36:45 +0800

    add example

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/refactor/EnumTest.java
@@ -0,0 +1,54 @@
+/*
+ * module: fundermental
+ * file: EnumTest.java
+ * date: 11/20/18 6:02 PM
+ * author: VectorJu
+ * copyright: (c) 2018 www.onechain001.com Inc. All rights reserved.
+ * 注意：本内容仅限于上海旺链信息科技有限公司内部传阅，禁止外泄以及用于其他的商业目的，否则将依法追责。
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-11-20 18:01
+ * @desc test
+ **/
+package com.xlab.service_java_infrastructure.refactor;
+
+public enum  EnumTest {
+    BUY(1,"buy"),
+    SELL(2,"sell");
+
+    private Integer code;
+    private String description;
+
+    EnumTest(Integer code,String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static EnumTest getByCode(int code) {
+        for (EnumTest enumTest:values()) {
+            if (enumTest.code == code) {
+                return enumTest;
+            }
+        }
+        return null;
+    }
+}
+
--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/refactor/Pig.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/refactor/Pig.java
@@ -12,11 +12,39 @@
  **/
 package com.xlab.service_java_infrastructure.refactor;
 
+import java.math.BigDecimal;
+
 public class Pig implements Animal{
 
     @Override
     public void show() {
         System.out.println("this is pig class ");
+//        BigDecimal bigDecimal = new BigDecimal(Long.valueOf("11.00000000"));
+        BigDecimal bigDecimal1 = new BigDecimal("11.00000000");
+//        System.out.println(" bigDecimal " + bigDecimal.toPlainString());
+//        System.out.println(" bigDecimal " + bigDecimal.toString());
+        System.out.println(" bigdecimal1 " + bigDecimal1.toString());
+        System.out.println(" bigdecimal1 " + bigDecimal1.toPlainString());
+    }
+
+
+    public static void main(String[] args) {
+        String str = "first";
+        boolean booleanSecond = false;
+        boolean booleanTird = false;
+
+        for(int i=0;i<3;i++) {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if ("first".equals(str) && !booleanSecond && !booleanTird) {
+                        System.out.println(" this is test : " + !booleanSecond + !booleanTird);
+                    }
+                }
+            }).start();
+
+        }
     }
 }
 
