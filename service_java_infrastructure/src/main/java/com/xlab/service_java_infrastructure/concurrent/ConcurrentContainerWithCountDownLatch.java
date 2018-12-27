31a619e7f7283d699dccbf398e91bb7b4669184c
居洪涛 <hongtao.ju@blockchain001.com>
2018-12-27 10:22:24 +0800

    add reentrantlock example

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/concurrent/ConcurrentContainerWithCountDownLatch.java
@@ -0,0 +1,61 @@
+/*
+ * module: fundermental
+ * file: ConcurrentContainerWithCountDownLatch.java
+ * date: 12/27/18 10:01 AM
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-12-27 10:01
+ * @desc count down latch simulator
+ **/
+package com.xlab.service_java_infrastructure.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class ConcurrentContainerWithCountDownLatch {
+
+    public static void main(String[] args) {
+
+        final ConcurrentContainerOrigin concurrentContainerOrigin = new ConcurrentContainerOrigin();
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (concurrentContainerOrigin.size() != 5) {
+                    try {
+                        countDownLatch.await();
+                    }catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(" size is 5 ");
+            }
+
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (countDownLatch){
+                    for (int i=0;i<10;i++) {
+                        System.out.println(" add start " + i);
+                        concurrentContainerOrigin.add(new Object());
+                        if (concurrentContainerOrigin.size() == 5) {
+                            countDownLatch.countDown();
+                        }
+                        try {
+                            TimeUnit.SECONDS.sleep(1);
+                        }catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+}
+
---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/concurrent/ConcurrentReentranlockSimulator.java
@@ -0,0 +1,70 @@
+/*
+ * module: fundermental
+ * file: ConcurrentReentranlockSimulator.java
+ * date: 12/27/18 10:15 AM
+ * author: VectorJu
+ */
+
+/**
+ * @author juhongtao
+ * @create 2018-12-27 10:15
+ * @desc concurrent reentrant lock simulation
+ **/
+package com.xlab.service_java_infrastructure.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentReentranlockSimulator {
+
+    Lock lock = new ReentrantLock();
+
+    void notifyMsg() {
+        try {
+
+            lock.lock();
+            for (int i=0;i<10;i++) {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println(" notifyMsg() " + i);
+            }
+
+
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    void notifyMsgDuplicate() {
+        lock.lock();
+        System.out.println("notifyMsgDuplicate() ");
+        lock.unlock();
+    }
+
+    public static void main(String[] args) {
+        final ConcurrentReentranlockSimulator concurrentReentranlockSimulator = new ConcurrentReentranlockSimulator();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                concurrentReentranlockSimulator.notifyMsg();
+            }
+        }).start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                concurrentReentranlockSimulator.notifyMsgDuplicate();
+            }
+        }).start();
+    }
+}
+
