bd211ad107c79655183a99fb433090c9987402c5
居洪涛 <hongtao.ju@blockchain001.com>
2019-03-18 10:36:02 +0800

    remove company declaration

--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/crypto/ASTOf.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/crypto/ASTOf.java
@@ -3,8 +3,6 @@
  * file: ASTOf.java
  * date: 3/15/19 4:51 PM
  * author: VectorJu
- * copyright: (c) 2018 www.onechain001.com Inc. All rights reserved.
- * 注意：本内容仅限于上海旺链信息科技有限公司内部传阅，禁止外泄以及用于其他的商业目的，否则将依法追责。
  */
 
 package com.xlab.service_java_infrastructure.crypto;
