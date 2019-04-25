83a020ee85ce9f96c890883b25e38870e6027cef
居洪涛 <hongtao.ju@blockchain001.com>
2019-04-25 09:57:01 +0800

    add upload with progress

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/realworld/DownloadWithProgressShow.java
@@ -0,0 +1,87 @@
+/*
+ * module: fundermental
+ * file: DownloadWithProgressShow.java
+ * date: 4/22/19 11:13 AM
+ * author: VectorJu
+ */
+
+/**
+ * @create 2019-04-22 11:13
+ * @desc test  download show progress
+ **/
+package com.xlab.service_java_infrastructure.realworld;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadWithProgressShow {
+
+
+    public static void dowanload(String url, String path)
+            throws IOException {
+        System.out.println("下载中...");
+        InputStream inputStream = null;
+        RandomAccessFile randomAccessFile = null;
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) new URL(url).openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setConnectTimeout(10 * 1000);
+            File file = new File(path);
+            //文件夹是否存在
+            if (!file.getParentFile().exists())
+                file.getParentFile().mkdir();
+            if (file.exists())
+                file.delete();
+            file.createNewFile();
+
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode >= 200 && responseCode < 300) {
+                inputStream = urlConnection.getInputStream();
+                int len = 0;
+                byte[] data = new byte[4096];
+                //用于保存当前进度（具体进度）
+                int progres = 0;
+                //获取文件长度
+                int maxProgres = urlConnection.getContentLength();
+                randomAccessFile = new RandomAccessFile(file, "rwd");
+                //设置文件大小
+                randomAccessFile.setLength(maxProgres);
+                //将文件大小分成100分，每一分的大小为unit
+                int unit = maxProgres / 100;
+                //用于保存当前进度(1~100%)
+                int unitProgress = 0;
+                while (-1 != (len = inputStream.read(data))) {
+                    randomAccessFile.write(data, 0, len);
+                    progres += len;//保存当前具体进度
+                    int temp = progres / unit; //计算当前百分比进度
+                    if (temp >= 1 && temp > unitProgress) {//如果下载过程出现百分比变化
+                        unitProgress = temp;//保存当前百分比
+                        System.out.println("正在下载中..." + unitProgress + "%");
+                    }
+                }
+                inputStream.close();
+                System.out.println("下载完成...");
+            } else {
+                System.out.println("服务器异常...");
+            }
+        } finally {
+            if (null != inputStream) {
+                inputStream.close();
+            }
+            if (null != randomAccessFile) {
+                randomAccessFile.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String path = "/Users/juhongtao/image.jpg";
+        String url="http://www.dowei.com/d/file/tuku/meinv/2016-01-26/1453788622507000.jpg";
+        dowanload(url, path);
+    }
+}
+
