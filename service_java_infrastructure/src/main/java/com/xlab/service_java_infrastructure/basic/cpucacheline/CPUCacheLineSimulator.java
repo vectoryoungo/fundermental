025568d96a22fd8e43087cbb28bbe7cf6ea67273
居洪涛 <hongtao.ju@blockchain001.com>
2019-07-17 16:20:38 +0800

    add cpucacheline package

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/basic/cpucacheline/CPUCacheLineSimulator.java
@@ -0,0 +1,53 @@
+/*
+ * module: fundermental
+ * file: CPUCacheLineSimulator.java
+ * date: 7/17/19 4:03 PM
+ * author: VectorJu
+ */
+
+/**
+ * @create 2019-07-17 16:03
+ * @desc simulate cpu cacheline test
+ * this illustrate how cache line work fast or not
+ * horizontalLoop takes 427ms
+ * landscapeLoop takes 1597ms
+ * my mac is 2.2 GHz Intel Core i7 4core 8 hyperThread
+ * my mac memory is 16 GB 1600 MHz DDR3
+ **/
+package com.xlab.service_java_infrastructure.basic.cpucacheline;
+
+import java.util.concurrent.TimeUnit;
+
+public class CPUCacheLineSimulator {
+
+
+    private static void horizontalLoop(int[][] array) {
+        // 横向遍历
+        for(int i = 0; i < 64 * 1024; i ++)
+            for(int j = 0; j < 1024; j ++)
+                array[i][j] ++;
+    }
+
+    private static void landscapeLoop(int[][] array) {
+        // 纵向遍历
+        for(int i = 0; i < 1024; i ++)
+            for(int j = 0; j < 64 * 1024; j ++)
+                array[j][i] ++;
+    }
+
+    public static void main(String[] args) {
+
+        long start = System.nanoTime();
+        int[][] array = new int[64 * 1024][1024];
+        if (args[0].equalsIgnoreCase("horizon")) {
+            horizontalLoop(array);
+        }else if (args[1].equalsIgnoreCase("landscape")) {
+            landscapeLoop(array);
+        }
+
+        long estimatedTime = System.nanoTime() - start;
+        System.out.println(" it takes " + TimeUnit.NANOSECONDS.toMillis(estimatedTime));
+
+    }
+}
+
