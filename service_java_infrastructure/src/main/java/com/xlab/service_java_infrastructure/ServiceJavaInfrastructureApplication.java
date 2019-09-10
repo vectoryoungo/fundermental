7fab99140ff2565518f8498fd7492b404c7fdb46
居洪涛 <hongtao.ju@blockchain001.com>
2019-09-10 16:34:50 +0800

    add springApplicationRun class ConfigurableApplicationContext.close method

--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/ServiceJavaInfrastructureApplication.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/ServiceJavaInfrastructureApplication.java
@@ -21,5 +21,6 @@ public class ServiceJavaInfrastructureApplication {
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(ServiceJavaInfrastructureApplication.class, args);
 		Person person = (Person) configurableApplicationContext.getBean("person");
 		System.out.println(person.getName());
+		configurableApplicationContext.close();
 	}
 }
