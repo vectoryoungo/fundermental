4d2540665b8c8e2784fcb1238cc0aab0f2509d51
juhongtao <ju.hongtao@oyohotels.cn>
2019-11-29 16:20:46 +0800

    remove comment

--- service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/bitcoinj/trigger/HDWalletTrigger.java
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/bitcoinj/trigger/HDWalletTrigger.java
@@ -3,11 +3,9 @@ package com.xlab.service_java_infrastructure.bitcoinj.trigger;
 import com.xlab.service_java_infrastructure.bitcoinj.BTCAccountPOJO;
 import com.xlab.service_java_infrastructure.bitcoinj.HDWalletService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Controller
 @RestController
 public class HDWalletTrigger {
 
