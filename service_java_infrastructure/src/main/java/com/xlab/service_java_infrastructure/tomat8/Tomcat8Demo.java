9963828fb5f16d77aa2e03383575812daea2fd16
juhongtao <ju.hongtao@oyohotels.cn>
2019-12-02 10:24:09 +0800

    add Tomcat8Demo

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/tomat8/Tomcat8Demo.java
@@ -0,0 +1,11 @@
+package com.xlab.service_java_infrastructure.tomat8;
+
+/**
+ *  for tomcat8 feature explore and learning
+ */
+public class Tomcat8Demo {
+
+    public static void main(String[] args) {
+
+    }
+}
