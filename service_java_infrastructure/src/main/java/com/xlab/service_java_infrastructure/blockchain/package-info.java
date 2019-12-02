685f7eea7ac1be99d082e61dd719eff4274c42df
juhongtao <ju.hongtao@oyohotels.cn>
2019-12-02 10:31:53 +0800

    add blockchain and wallet package

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/blockchain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * for blockchain explore only
+ */
+package com.xlab.service_java_infrastructure.blockchain;
---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/blockchain/wallet/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * for currency wallet explore only
+ */
+package com.xlab.service_java_infrastructure.blockchain.wallet;
