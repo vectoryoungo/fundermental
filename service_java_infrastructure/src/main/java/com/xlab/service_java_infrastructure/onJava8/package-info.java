55eda5cdf00ad3bca36300b4940602ee0276b0c9
居洪涛 <hongtao.ju@blockchain001.com>
2020-04-19 21:58:35 +0800

    add onJava8

---
+++ service_java_infrastructure/src/main/java/com/xlab/service_java_infrastructure/onJava8/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * module: fundermental
+ * file: package-info.java
+ * date: 4/19/20 9:49 PM
+ * author: VectorJu
+ */
+
+/**
+ * @create 2020-04-19 21:48
+ * @desc OnJava8 book
+ **/
+package com.xlab.service_java_infrastructure.onJava8;
